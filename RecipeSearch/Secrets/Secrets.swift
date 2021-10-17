9da0c68ddae5137a2a1ef08a9b6084a9f69af84b
chakane <chakanezshegog@gmail.com>
2021-10-16 20:30:37 -0400

    added secret folder

---
+++ RecipeSearch/Secrets/Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  Secrets.swift
+//  RecipeSearch
+//
+//  Created by Chakane Shegog on 10/16/21.
+//  Copyright © 2021 Alex Paul. All rights reserved.
+//
+
+import Foundation
+
+struct SecretKey {
+    
+}
